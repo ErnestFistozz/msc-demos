2defa49c8c439dd2a34f540b0e9a16cd3749f9de
Ernest Mamba <ebmamba@shoprite.co.za>
2023-10-25 23:05:42 +0200

    adding test cases after code being written

--- test_calculator.py
+++ test_calculator.py
@@ -5,9 +5,13 @@ class TestCalculator(unittest.TestCase):
 
     def test_divide(self):
         self.assertEqual(2.0,Calculator.divide(4,2))
+        
+    # raises ZeroDivisionError exception
+    def test_divide_raises_exception(self):
+        self.assertRaises(ZeroDivisionError, Calculator.divide, 5, 0)
 
     def test_subtract(self):
         self.assertEqual(4, Calculator.subtract(3,7))
-        
+
 if __name__ == '__main__':
     unittest.main()
