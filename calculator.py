e7e2b8daaedcd7a29d424970c5b5a42e2b7a1156
Ernest Mamba <ebmamba@shoprite.co.za>
2023-10-25 22:50:27 +0200

    adding subtraction tests - but ignoring test case for exception

--- calculator.py
+++ calculator.py
@@ -1,7 +1,14 @@
 
 
 class Calculator:
+    # method to divide two numbers
     @staticmethod
     def divide(a: int, b: int) -> float:
+        # raise an exception if the denominator is zero
+        if b == 0:
+            raise ZeroDivisionError
         return a/b
-    
+    # method to subtract two numbers
+    @staticmethod
+    def subtract(a: int, b: int) -> int:
+        return b - a
--- test_calculator.py
+++ test_calculator.py
@@ -6,6 +6,8 @@ class TestCalculator(unittest.TestCase):
     def test_divide(self):
         self.assertEqual(2.0,Calculator.divide(4,2))
 
-
+    def test_subtract(self):
+        self.assertEqual(4, Calculator.subtract(3,7))
+        
 if __name__ == '__main__':
     unittest.main()
